d52cbb8977d0f2441b4fc6d511bfc74d77cbd679
Jun <jun.kim.dev@gmail.com>
2018-08-24 10:31:20 -0400

    fixed location.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Location, Permissions } from 'expo'
 
 import Weather from './Weather';
 
@@ -16,22 +17,53 @@ export default class App extends React.Component {
   };
 
   componentDidMount(){
-    navigator.geolocation.getCurrentPosition( 
-      (position) =>{
-        this.setState({
-        });
-        this._getWeather(position.coords.latitude, position.coords.longitude);
-      }, (error) =>{
-        this.setState({
-          isLoaded: false,
-          error: error.message
-        });
-      }   
-   );
+    console.log("componentDidMount start");
+    this._getLocationAsync();
+//     navigator.geolocation.getCurrentPosition( 
+//       (position) =>{
+// //        this.setState({        });
+//           console.log("position");
+
+//         this._getWeather(position.coords.latitude, position.coords.longitude);
+//       }, (error) =>{
+//         console.log(error.message);
+//         this.setState({
+//           isLoaded: false,
+//           error: error.message
+//         }, 
+//         {enableHighAccuracy: true, timeout: 20000, maximumAge: 10000});
+//       }   
+//    );
+   console.log("componentDidMount- end");
 
   }
 
+  _getLocationAsync = async () => {
+    console.log("_getLocationAsync");
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    console.log(status);
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      // this.setState({
+      //   locationResult: 'Permission to access location was denied',
+      // });
+    }
+    console.log("_getLocationAsync 2");
+ 
+    let location = await Location.getCurrentPositionAsync({});
+
+//    {"mocked":false, "timestamp":1534821223000,
+//      "coords":{ "speed":0,"heading":0,"accuracy":20,"longitude":-122.084,"altitude":0,"latitude":37.4219983 }
+      // }
+
+    this._getWeather(location.coords.latitude, location.coords.longitude);
+
+    console.log(location);
+
+  };
+ 
   _getWeather = (lat, lon) => {
+    console.log("_getWeather");
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
     .then(response => response.json())
     .then(json => {
@@ -40,6 +72,7 @@ export default class App extends React.Component {
         name: json.weather[0].main,
         isLoaded: true
       })
+      console.log(json);
       console.log(this.state.temperature + ", " + this.state.name);
     });
   }
